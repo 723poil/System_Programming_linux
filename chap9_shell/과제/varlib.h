80d7b9b5af84534bbc23dc14db71ed98643eb902
723poil <leetkdguq73@naver.com>
2021-11-12 18:56:34 +0900

    과제

---
+++ chap9_shell/과제/varlib.h
@@ -0,0 +1,7 @@
+int builtin_command(char **, int *);
+int VLstore(char *, char *);
+char* VLlookup(char *);
+int VLexport(char *);
+void VLlist();
+int VLenviron2table(char**);
+char** VLtable2environ();
