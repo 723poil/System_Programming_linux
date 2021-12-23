dd5490fc38c3c1c918ae36c8aa6c1aee4bd62031
723poil <75459370+723poil@users.noreply.github.com>
2021-12-23 21:27:25 +0900

    Update main.c

--- project/main.c
+++ project/main.c
@@ -102,7 +102,7 @@ void QUIT_handler() {
 			dup2(thepipe[1], 1);
 			close(thepipe[1]);
 
-			write(stdout, 11+ctime(&info.st_mtim), 5);
+			write(stdout, 10+ctime(&info.st_mtim), 5);
 		}
 		
 		tty_mode(1);
